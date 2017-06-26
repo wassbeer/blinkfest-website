419311e0bb8e5642923dc29a9c49c1e2ffe0638a
wassbeer <tm.wassenberg@gmail.com>
2017-06-26 20:39:10 +0200

    info page with ticket buy and map

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){ 
 
+var x = 1; 
+var y = 1;
+
+
  $("a").hover(
 // function 1 of 2 is declared for when mouse enters
 	function() {
@@ -14,4 +18,50 @@ $(document).ready(function(){
 	// $(this).fadeIn(5000);
 	$(this).css("text-decoration", "none");
 	});
+
+  $(".backgroundother").mouseenter(
+	function() {
+	x = x + 1;
+  	console.log(x);
+  	if(x%2===0){
+	$(this).css("background", "url('images/noninvert.png')");
+	// }
+}else{$(this).css("background", "url('images/invert.png')")}}
+	);
+
+  $(".backgroundone").mouseenter(
+// function 1 of 2 is declared for when mouse enters
+	function() {
+	y = y + 1;
+  	console.log(y);
+  	if(y%2===0){
+	$(this).css("background", "url('images/invert.png')");
+	// }
+}else{$(this).css("background", "url('images/noninvert.png')")}}
+	);
+
+
+
 	}); 
+
+// (function(){
+ 
+//   $("#cart").on("click", function() {
+//     $(".shopping-cart").fadeToggle( "fast");
+//   });
+  
+// })();
+
+// var sourceSwap = function () {
+//     var $this = $(this);
+//     var newSource = $this.data('alt-src');
+//     $this.data('alt-src', $this.attr('src'));
+//     $this.attr('src', newSource);
+// }
+
+// $(function() {
+//     $('img[data-alt-src]').each(function() { 
+//         new Image().src = $(this).data('alt-src'); 
+//     }).hover(sourceSwap, sourceSwap); 
+// });
+
